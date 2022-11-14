2f0259732ff6de8d41fee4f3827d855586f268f2
Neil <barrymun@tcd.ie>
2022-11-14 14:24:33 +0000

    btn, btn style + onclick method [wip]

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+const toggleNav = (e: MouseEvent) => {
+  console.log(e)
+};
+
+const navToggleBtn: HTMLElement | null = document.getElementById("nav-toggle");
+
+if (navToggleBtn != null) navToggleBtn.onclick = toggleNav;
+
 export {};
